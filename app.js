817a269169be6da74eef09d7b161bb10cb7af4c3
anga13 <kalle.spik@hotmail.com>
2019-05-23 13:36:02 +0200

    Edit app

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express = require('express')
 const app = express()
 
-app.get('/employees', () => {
-	res.status(200)
+app.get('/employees', (req, res) => {
+	res.json(employees)
+})
+.get('/employees/:id', (req, res) => {
+	const employee = findOne(req.params.id)
+	res.json(employee)
 })
 
 module.exports = app
 
+employees = [
+	{id:1, name: 'Anders'},
+	{id:2, name: 'Anna'},
+	{id:3, name: 'Knut'},
+	{id:4, name: 'Stina'}
+]
+
+const findOne = id => {
+	let index = id -1
+	return employees[index]
+}
